dc73fd4209c18141b7ac4cb585f08ae5498a9eb4
ChiaraR3 <chiara.r.3@live.it>
2021-09-08 19:20:26 +0000

    iniciar

--- src/models.py
+++ src/models.py
@@ -1,12 +1,53 @@
 from flask_sqlalchemy import SQLAlchemy
+  
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
+
+Base = declarative_base()
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+   
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    hair_color = Column(String(250))
+    homeworld = Column(String(250))
+    eye_color = Column(String(250))
+    gender = Column(String(250)) 
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gravity = Column(String(250)) 
+    population = Column(String(250)) 
+    climate = Column(String(250))
+    
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    people_id = Column(Integer, ForeignKey('people.id'))
+    people = relationship(People)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
 
     def __repr__(self):
         return '<User %r>' % self.username
